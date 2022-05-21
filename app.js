e7890b94cadd7424fdef0704231e7f76cb8261f3
Martiz <>
2022-05-21 17:47:14 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Hello");
+
+const userInputs = [];
+
+//console.log('In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[1]}. Word got out that the team had "debugged" the ${userInputs[1]}')
